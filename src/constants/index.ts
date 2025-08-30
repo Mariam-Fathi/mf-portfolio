c235aa705c1257466efcc369912027ac81e9d6ae
Mariam Fathi <mariam.f.siam@gmail.com>
2025-08-31 00:01:27 +0300

    feat: add responsive hero parallax component

---
+++ src/constants/index.ts
@@ -0,0 +1,80 @@
+export const projects = [
+  {
+    title: "Moonbeam",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+  {
+    title: "Cursor",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+  {
+    title: "Rogue",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+
+  {
+    title: "Editorially",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+  {
+    title: "Editrix AI",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+
+  {
+    title: "Algochurn",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+
+  {
+    title: "Creme Digital",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF2.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "https://sell-myself.vercel.app/",
+    thumbnail: "./MF1.png",
+  },
+];
