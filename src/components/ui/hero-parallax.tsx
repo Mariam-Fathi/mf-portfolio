ae6588e8bcc58bb173b443f96a08845ce8078531
Mariam Fathi <mariam.f.siam@gmail.com>
2025-08-31 00:15:46 +0300

    fix: change font size

--- src/components/ui/hero-parallax.tsx
+++ src/components/ui/hero-parallax.tsx
@@ -102,12 +102,12 @@ export const HeroParallax = ({
 export const Header = () => {
   return (
     <div className="max-w-7xl relative mx-auto py-20 md:py-40 px-4 w-full left-0 top-0">
-      <h1 className="text-2xl md:text-7xl font-bold dark:text-white">
+      <h1 className="text-4xl md:text-7xl font-bold dark:text-white">
         From Data
         <br />
         to Intelligent Systems
       </h1>
-      <p className="max-w-2xl text-base md:text-xl mt-8 dark:text-neutral-200">
+      <p className="max-w-2xl text-base sm:text-xl mt-8 dark:text-neutral-200">
         I combine data engineering and frontend development to explore how
         patterns emerge from complexity. My goal is to advance from building
         systems that display data to designing systems that learn from it —
