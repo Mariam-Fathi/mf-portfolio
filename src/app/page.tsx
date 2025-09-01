04b26e143402f800da7f97c6bbd0c9f5f644ae08
Mariam Fathi <mariam.f.siam@gmail.com>
2025-09-02 01:48:50 +0300

    feat: add hero animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,7 @@
 "use client";
 
-import { ContainerScroll } from "@/components/ui/container-scroll-animation";
+import { HeroAnimation } from "@/components/ui/hero-animation";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col overflow-hidden">
-      <ContainerScroll
-        titleComponent={
-          <>
-            <h1 className="text-4xl font-semibold text-white">
-              Portfolio <br />
-              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
-                Mariam Fathi{" "}
-              </span>
-            </h1>
-          </>
-        }
-      >
-        <img
-          src={`/MF2.png`}
-          alt="hero"
-          height={720}
-          width={1400}
-          className="mx-auto rounded-2xl object-cover h-full object-left-top"
-          draggable={false}
-        />
-      </ContainerScroll>
-    </div>
-  );
+  return <HeroAnimation />;
 }
---
+++ src/components/ui/hero-animation.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { motion } from "motion/react";
+
+export function HeroAnimation() {
+  return (
+    <div className="relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center">
+      <Navbar />
+      <div className="absolute inset-y-0 left-0 h-full w-px bg-neutral-800/80">
+        <div className="absolute top-0 h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
+      </div>
+      <div className="absolute inset-y-0 right-0 h-full w-px bg-neutral-800/80">
+        <div className="absolute h-40 w-px bg-gradient-to-b from-transparent via-blue-500 to-transparent" />
+      </div>
+      <div className="absolute inset-x-0 bottom-0 h-px w-full bg-neutral-800/80">
+        <div className="absolute mx-auto h-px w-40 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
+      </div>
+      <div className="px-4 py-10 md:py-20">
+        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold  md:text-4xl lg:text-7xl text-slate-300">
+          {"From Data to Intelligent Systems".split(" ").map((word, index) => (
+            <motion.span
+              key={index}
+              initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+              animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+              transition={{
+                duration: 0.3,
+                delay: index * 0.1,
+                ease: "easeInOut",
+              }}
+              className="mr-2 inline-block"
+            >
+              {word}
+            </motion.span>
+          ))}
+        </h1>
+        <motion.p
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 0.8,
+          }}
+          className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-400"
+        >
+          I combine data engineering and frontend development to explore how
+          patterns emerge from complexity. My goal is to advance from building
+          systems that display data to designing systems that learn from it —
+          developing adaptive, intelligent solutions that bridge technology and
+          human insight. up.
+        </motion.p>
+
+        <motion.div
+          initial={{
+            opacity: 0,
+            y: 10,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 1.2,
+          }}
+          className="relative z-10 mt-10 rounded-3xl border  p-4 shadow-md border-neutral-800 bg-neutral-900"
+        >
+          <div className="w-full overflow-hidden rounded-xl border border-gray-700">
+            <img
+              src="/MF1.png"
+              alt="Landing page preview"
+              className="aspect-[16/9] h-auto w-full object-cover"
+              height={1000}
+              width={1000}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </div>
+  );
+}
+
+const Navbar = () => {
+  return (
+    <nav className="flex w-full items-center justify-between border-t border-b  px-4 py-4 border-neutral-800 text-white">
+      <div className="flex items-center gap-2">
+        <div className="size-7 rounded-full bg-gradient-to-br from-green-950 to-green-500" />
+        <h1 className="text-base font-bold md:text-2xl">
+          Mariam Fathi Portfolio
+        </h1>
+      </div>
+    </nav>
+  );
+};
