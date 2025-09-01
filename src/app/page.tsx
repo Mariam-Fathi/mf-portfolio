5f368fad54e1cbd43e52ebca127b21a6dd6ded2c
Mariam Fathi <mariam.f.siam@gmail.com>
2025-09-02 00:54:11 +0300

    feat: add container scroll animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 "use client";
 
-import { HeroParallax } from "@/components/ui/hero-parallax";
-import { TracingBeam } from "@/components/ui/tracing-beam";
-import { projects } from "@/constants";
+import { ContainerScroll } from "@/components/ui/container-scroll-animation";
 
 export default function Home() {
   return (
-    <TracingBeam>
-      <HeroParallax projects={projects} />
-    </TracingBeam>
+    <div className="flex flex-col overflow-hidden">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-4xl font-semibold text-white">
+              Portfolio <br />
+              <span className="text-4xl md:text-[6rem] font-bold mt-1 leading-none">
+                Mariam Fathi{" "}
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <img
+          src={`/MF2.png`}
+          alt="hero"
+          height={720}
+          width={1400}
+          className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          draggable={false}
+        />
+      </ContainerScroll>
+    </div>
   );
 }
