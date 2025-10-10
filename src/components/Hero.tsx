5bd7699b85bca143d4678efc4475e9b1d569b55d
Mariam Fathi <mariam.f.siam@gmail.com>
2025-10-10 19:39:21 +0300

    add padding bottom hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,10 @@ import {GoogleGeminiEffectDemo} from "@/components/Curiosity";
 
 const Hero = () => {
     return (
+        <section className={'pb-10'}>
             <GoogleGeminiEffectDemo />
+
+        </section>
     );
 };
 
