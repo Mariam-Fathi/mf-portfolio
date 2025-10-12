a82aa3a5d3d4d50efd1be2cf70439cfc2bc4c5e8
Mariam Fathi <mariam.f.siam@gmail.com>
2025-10-12 12:40:30 +0300

    improve hero section rendering and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
+import Hero from "@/components/hero/hero";
 import {Projects} from "@/components/Projects";
 import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { GoogleGeminiEffectComponent } from "@/components/hero/google-gemini-effect-component";
+
+const Hero = () => {
+  return <GoogleGeminiEffectComponent />;
+};
+
+export default Hero;
