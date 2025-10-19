dc4b7c23287a3bcefac73a16216fa29820e9cf37
Mariam Fathi <mariam.f.siam@gmail.com>
2025-10-19 17:25:32 +0300

    update contact section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,11 @@
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="justify-center">
-                <div className="flex flex-col justify-center">
-                    <p className="text-center md:text-end">
+            <div className="justify-between w-full flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="w-4"/>
+                     <p className="">
                         © {new Date().getFullYear()} Mariam Fathi All rights reserved.
                     </p>
-                </div>
             </div>
         </footer>
     );
