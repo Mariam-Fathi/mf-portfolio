a20746ce1ea680fd8d9ecf134c90ab4357b401b9
Mariam Fathi <mariam.f.siam@gmail.com>
2025-10-21 07:40:58 +0300

    update style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="justify-between w-full flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="w-4"/>
-                     <p className="">
-                        © {new Date().getFullYear()} Mariam Fathi All rights reserved.
-                    </p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="justify-between w-full flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="w-4" />
+        <p className="text-gray-700">
+          © {new Date().getFullYear()} Mariam Fathi All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
