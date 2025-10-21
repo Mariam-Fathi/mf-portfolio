21afcfd5e6016a2273781926f3892db9e851344c
Mariam Fathi <mariam.f.siam@gmail.com>
2025-10-21 07:44:40 +0300

    update style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="footer">
       <div className="justify-between w-full flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="w-4" />
-        <p className="text-gray-700">
+        <p className="text-white">
           © {new Date().getFullYear()} Mariam Fathi All rights reserved.
         </p>
       </div>
